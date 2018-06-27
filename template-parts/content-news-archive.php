a68457f25f764bcf80282d35ada64d7b9ef69569
Josh Armentano <josh@abidewebdesign.com>
2018-06-27 14:29:12 -0700

    Style changes

--- template-parts/content-news-archive.php
+++ template-parts/content-news-archive.php
@@ -12,9 +12,7 @@ endif;
 <div class="news-item-list margin-bottom-one">
 	<div class="row news-item padding-bottom-one">
 		<div class="col-sm-12">
-			<a href="<?php echo $link; ?>" <?php if ( get_field('news_post_source') == 'External' ): ?> target="_blank" <?php endif; ?>>
-				<h3 class="news-item-title"><?php the_title(); ?></h3>
-			</a>
+			<h3 class="news-item-title"><a href="<?php echo $link; ?>" <?php if ( get_field('news_post_source') == 'External' ): ?> target="_blank" <?php endif; ?>><?php the_title(); ?></h3></a>
 			<div class="post-meta post-meta-sm">
 				<span id="post-date"><?php the_time('F j, Y'); ?>,</span>
 				<?php if ( get_field('news_post_source') != 'External' ): ?><span id="post-author"> by <?php the_author(); ?></span><?php endif; ?>
