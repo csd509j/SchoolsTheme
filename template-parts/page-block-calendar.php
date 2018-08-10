ca2bb9aa5b92bc87dcc1a67d03f2b611450eb75f
Josh Armentano <josh@abidewebdesign.com>
2018-08-10 09:26:15 -0700

    Removed tribe events calendar files

--- template-parts/page-block-calendar.php
+++ template-parts/page-block-calendar.php
@@ -1,4 +1,5 @@
 <?php $cat = get_sub_field('calendar_category'); ?>
+<!--
 <div class="padding-bottom-two">
-	<?php echo do_shortcode('[tribe_events_list limit="1000" category="#' . $cat . '"]'); ?>
 </div>
+-->
