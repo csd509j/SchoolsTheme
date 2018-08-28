ded56dd63df474914931bbd9047210f0e0e2e1f1
Josh Armentano <josh@abidewebdesign.com>
2018-08-28 08:43:03 -0700

    Update: Page builder calendar component

--- template-parts/page-block-calendar.php
+++ template-parts/page-block-calendar.php
@@ -1,5 +1,7 @@
-<?php $cat = get_sub_field('calendar_category'); ?>
-<!--
-<div class="padding-bottom-two">
+<?php $calendar_address = get_sub_field('calendar_address'); ?>
+<div class="padding-bottom-one">
+	<?php if(get_sub_field('calendar_title')): ?>
+		<h2><?php the_sub_field('calendar_title'); ?></h2>
+	<?php endif; ?>
+	<?php render_page_builder_calendar($calendar_address); ?>
 </div>
--->
