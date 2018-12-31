9b9e9ca8e90c1e2c9feeecdd75d36de9f06c7d2c
Josh Armentano <josh@abidewebdesign.com>
2018-12-31 14:28:52 -0800

    Fixed: Bug with search results returning no results.

--- search.php
+++ search.php
@@ -17,53 +17,51 @@ get_header();
 			<div class="col-12">
 				<h2 class="mb-1">Search</h2>
 				<div class="bg-gray p-1">
-					<div class="well">
-						<div id="search-form">
-							<form role="search" id="sites-search" class="row no-gutters" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-								<div class="form-group col-md-9 col-lg-10 mb-1 mb-md-0">
-									<label class="sr-only" for="search-text">Search</label>
-									<input type="text" class="form-control-lg w-100" id="search-text" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
-								</div>
-								<div class="col-md-3 col-lg-2 d-flex align-items-stretch">
-									<button type="submit" class="btn btn-primary btn-block">Search</button>
-								</div>
-							</form>
-						</div>	
-					</div>
+					<div id="search-form">
+						<form role="search" id="sites-search" class="row no-gutters" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+							<div class="form-group col-md-9 col-lg-10 mb-1 mb-md-0">
+								<label class="sr-only" for="search-text">Search</label>
+								<input type="text" class="form-control-lg w-100" id="search-text" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
+							</div>
+							<div class="col-md-3 col-lg-2 d-flex align-items-stretch">
+								<button type="submit" class="btn btn-primary btn-block">Search</button>
+							</div>
+						</form>
+					</div>	
 				</div>
 			</div>
 		</div>
-		<?php if ( have_posts() ) : ?>
-	
 		<div class="row justify-content-center">
-			<div class="col-lg-8">
-				<?php
-				// Start the loop.
-				while ( have_posts() ) : the_post();
-	
-					/**
-					 * Run the loop for the search to output the results.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-search.php and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', 'search' );
-	
-				// End the loop.
-				endwhile;
-	
-				// Previous/next page navigation.
-				show_pagination_links( );
+			<div class="col-md-9">
+				<?php 
 
+				if ( have_posts() ) :
+			
+					while ( have_posts() ) : the_post();
+		
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'search' );
+		
+					// End the loop.
+					endwhile;
+		
+					// Previous/next page navigation.
+					show_pagination_links( );
 	
-			// If no content, include the "No posts found" template.
-			else :
-				echo "No results returned.";
-	
-			endif;
+		
+				// If no content, include the "No posts found" template.
+				else :
+					echo "No results returned.";
+		
+				endif;
 			?>
 			</div>
-			<div class="col-md-6 col-lg-4">
-				<div class="bg-gray p-1 text-center">
+			<div class="col-md-3">
+				<div class="bg-gray p-1 mt-2 mt-md-0 text-center">
 					<h3><?php the_field('search_sidebar_title', 'options'); ?></h3>
 					<p class="small mb-1"><?php the_field('search_sidebar_text', 'options'); ?></p>
 					<a class="btn btn-primary btn-sm btn-block" href="<?php echo home_url(); ?>/contact">Contact Us</a>
