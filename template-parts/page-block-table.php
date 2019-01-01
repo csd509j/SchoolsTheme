8260bc601e085b8156002ea8b8e70b8c87ca52b0
Josh Armentano <josh@abidewebdesign.com>
2019-01-01 09:12:16 -0800

    Fixed: Bug with responsive tables.

--- template-parts/page-block-table.php
+++ template-parts/page-block-table.php
@@ -2,45 +2,48 @@
 $table = get_sub_field( 'table' );
 
 if ( $table ) {
-
-    echo '<table class="table table-responsive table-striped table-bordered margin-bottom-one">';
-
-        if ( $table['header'] ) {
-
-            echo '<thead>';
-
-                echo '<tr>';
-
-                    foreach ( $table['header'] as $th ) {
-
-                        echo '<th>';
-                            echo $th['c'];
-                        echo '</th>';
-                    }
-
-                echo '</tr>';
-
-            echo '</thead>';
-        }
-
-        echo '<tbody>';
-
-            foreach ( $table['body'] as $tr ) {
-
-                echo '<tr>';
-
-                    foreach ( $tr as $td ) {
-
-                        echo '<td>';
-                            echo $td['c'];
-                        echo '</td>';
-                    }
-
-                echo '</tr>';
-            }
-
-        echo '</tbody>';
-
-    echo '</table>';
+	echo '<div class="table-responsive">';
+	    
+	    echo '<table class="table table-striped table-bordered margin-bottom-one">';
+	
+	        if ( $table['header'] ) {
+	
+	            echo '<thead>';
+	
+	                echo '<tr>';
+	
+	                    foreach ( $table['header'] as $th ) {
+	
+	                        echo '<th>';
+	                            echo $th['c'];
+	                        echo '</th>';
+	                    }
+	
+	                echo '</tr>';
+	
+	            echo '</thead>';
+	        }
+	
+	        echo '<tbody>';
+	
+	            foreach ( $table['body'] as $tr ) {
+	
+	                echo '<tr>';
+	
+	                    foreach ( $tr as $td ) {
+	
+	                        echo '<td>';
+	                            echo $td['c'];
+	                        echo '</td>';
+	                    }
+	
+	                echo '</tr>';
+	            }
+	
+	        echo '</tbody>';
+	
+	    echo '</table>';
+	  
+	echo '</div>';
 }
 ?>
