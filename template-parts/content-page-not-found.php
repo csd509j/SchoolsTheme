f4ae727f83251366bc516a147db2fec6bdaa4e64
Josh Armentano <josh@abidewebdesign.com>
2019-04-05 14:40:42 -0700

    Updates to 404 page.

--- template-parts/content-page-not-found.php
+++ template-parts/content-page-not-found.php
@@ -4,27 +4,23 @@ $links_col_1 = get_field('popular_pages_list', 'option');
 $links_col_2 = get_field('popular_resources_list', 'option'); 
 
 ?>
-<div class="row">
-	<div class="col-12">
+<div class="row justify-content-center">
+	<div class="col-lg-12 bg-light p-2 mb-2">
 		<h2><?php the_field('error_page_title', 'options'); ?></h2>
 		<div class="entry-lead">
 			<p class="lead"><?php the_field('error_page_message', 'options'); ?></p>
 		</div>
-		<div class="bg-gray p-1 mb-2">
-			<div class="well">
-				<div id="search-form">
-					<form role="search" id="sites-search" class="row no-gutters" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-						<div class="form-group col-md-9 col-lg-10 mb-1 mb-md-0">
-							<label class="sr-only" for="search-text">Search</label>
-							<input type="text" class="form-control-lg w-100" id="search-text" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
-						</div>
-						<div class="col-md-3 col-lg-2 d-flex align-items-stretch">
-							<button type="submit" class="btn btn-primary btn-block">Search</button>
-						</div>
-					</form>
-				</div>	
-			</div>
-		</div>
+		<div id="search-form">
+			<form role="search" id="sites-search" class="row no-gutters" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+				<div class="form-group col-md-9 col-lg-10 mb-1 mb-md-0">
+					<label class="sr-only" for="search-text">Search</label>
+					<input type="text" class="form-control-lg w-100" id="search-text" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
+				</div>
+				<div class="col-md-3 col-lg-2 d-flex align-items-stretch">
+					<button type="submit" class="btn btn-primary btn-block">Search</button>
+				</div>
+			</form>
+		</div>	
 	</div>
 </div>
 <div class="row entry-content padding-top-two">
@@ -32,7 +28,7 @@ $links_col_2 = get_field('popular_resources_list', 'option');
 		<h3>Popular Pages</h3>
 		<ul class="list list-flush">
 			<?php foreach($links_col_1 as $link): ?>
-				<li><a href="<?php echo $link->guid; ?>"><?php echo $link->post_title; ?></a></li>
+				<li><a href="<?php echo get_permalink($link); ?>"><?php echo get_the_title($link) ?></a></li>
 			<?php endforeach; ?>
 		</ul>
 	</div>
@@ -40,7 +36,7 @@ $links_col_2 = get_field('popular_resources_list', 'option');
 		<h3>Popular Resources</h3>
 		<ul class="list list-flush">
 			<?php foreach($links_col_2 as $link): ?>
-				<li><a target="_blank" href="<?php echo $link->guid; ?>"><?php echo $link->post_title; ?></a></li>
+				<li><a target="_blank" href="<?php echo get_permalink($link); ?>"><?php echo get_the_title($link) ?></a></li>
 			<?php endforeach; ?>
 		</ul>
 	</div>
