c301208ce0b6e45b93cc0e4234eba76159fcc756
Josh Armentano <josh@abidewebdesign.com>
2019-07-19 12:38:32 -0700

    Updates to page header.

--- page-language-selection.php
+++ page-language-selection.php
@@ -6,8 +6,7 @@
  * @subpackage CSD Schools
  * @since CSD Schools 1.7.5
  */
-wp_head(); 
-
+ 
 if (isset($_GET['referrer'])) {
 	$referrer = $_GET['referrer'];
 } else {
@@ -15,34 +14,68 @@ if (isset($_GET['referrer'])) {
 }
 
 ?>
-<div id="page-language">
-	<div class="container">
-		<div class="modal" id="lang-splash" tabindex="-1" role="dialog" aria-labelledby="lang-splash" aria-hidden="false">
-			<div class="modal-dialog">
-		    	<div class="modal-content">
-			    	<div class="modal-header d-flex">
-				    	<div class="d-flex justify-content-start">
-							<h3 class="modal-title">Preferred Language</h3>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+	
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=medium-dpi" />
+		<meta name="google-site-verification" content="" />
+		<?php
+		
+		if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) {
+		
+			echo('<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">');
+			
+		} 
+		
+		?>
+	
+		<title><?php wp_title(''); ?></title>
+	
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<link rel="shortcut icon" type="image/x-icon" href="<?php the_field('favicon', 'options'); ?>">
+		<?php get_template_part('template-parts/google', 'analytics'); ?>
+		<?php wp_head(); ?>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+	<body <?php body_class(); ?>>
+		<div id="page-language">
+			<div class="container">
+				<div class="modal" id="lang-splash" tabindex="-1" role="dialog" aria-labelledby="lang-splash" aria-hidden="false">
+					<div class="modal-dialog">
+				    	<div class="modal-content">
+					    	<div class="modal-header d-flex">
+						    	<div class="d-flex justify-content-start">
+									<h3 class="modal-title">Preferred Language</h3>
+						    	</div>
+							</div>
+							<div class="modal-body">
+								<div class="d-flex border-bottom">
+									<div class="flex-fill border-right">
+										<a href="#" class="lang-link" data-id="en" data-referrer="<?php echo $referrer; ?>"><img src="<?php echo home_url('wp-content/themes/csdschools/assets/images/american-flag.jpg'); ?>" width="45px" /> English</a>
+									</div>
+									<div class="flex-fill">
+										<a href="#" class="lang-link" data-id="es" data-referrer="<?php echo $referrer; ?>"><img src="<?php echo home_url('wp-content/themes/csdschools/assets/images/mexico-flag.jpg'); ?>" width="45px" /> Spanish</a>
+									</div>
+								</div>
+						   		<div class="my-1 text-center">
+							    	<img src="<?php the_field('logo', 'option'); ?>" class="img-fluid" width="250px" />	
+						        </div>
+				      		</div>
 				    	</div>
-					</div>
-					<div class="modal-body">
-						<div class="d-flex border-bottom">
-							<div class="flex-fill border-right">
-								<a href="#" class="lang-link" data-id="en" data-referrer="<?php echo $referrer; ?>"><img src="<?php echo home_url('wp-content/themes/csdschools/assets/images/american-flag.jpg'); ?>" width="45px" /> English</a>
-							</div>
-							<div class="flex-fill">
-								<a href="#" class="lang-link" data-id="es" data-referrer="<?php echo $referrer; ?>"><img src="<?php echo home_url('wp-content/themes/csdschools/assets/images/mexico-flag.jpg'); ?>" width="45px" /> Spanish</a>
-							</div>
-						</div>
-				   		<div class="my-1 text-center">
-					    	<img src="<?php the_field('logo', 'option'); ?>" class="img-fluid" width="250px" />	
-				        </div>
-		      		</div>
-		    	</div>
-		  	</div>
+				  	</div>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
+	</body>
+</html>
 <script>
 $(document).ready(function() {
 	$('.lang-link').click(function() {
