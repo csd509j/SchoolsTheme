4a9c6f2b7e3b003a6ab9bc3e8c30eba946869ce9
Josh Armentano <josh@abidewebdesign.com>
2020-03-24 10:53:41 -0700

    Removed external link functionality.

--- assets/js/core.js
+++ assets/js/core.js
@@ -26,23 +26,4 @@ jQuery(function ($) {
 		e.preventDefault(); // stop form submission
 	});
 	
-	// External Link Pop-up with domains to whitelist
- 	var domains = ['csd509j.net', 'https://teachcorvallis.org'];
-
-	$('a[href^="http"]').on('click', function (e) {
-		
-		var link = $(this).attr('href');
-		
-		var external = domains.find( function (domain) {
-			var reg = new RegExp( domain );
-			return link.match(reg) !== null;		
-		});
-
-		if ( external === undefined ) {
-			e.preventDefault();
-			$('#externalLink').attr('href', $(this).attr('href'));
-			$('#modalNotification').modal('show');
-		}
-				
-	});
 });
--- footer.php
+++ footer.php
@@ -155,24 +155,7 @@
 				</section>
 			</div>
 		</div>
-		<div class="modal fade" id="modalNotification" tabindex="-1" role="dialog" aria-labelledby="modalNotification" aria-hidden="true">
-			<div class="modal-dialog modal-sm" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="modalNotificationLable"><?php _e('Notice','csdschools'); ?></h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-						<p class="mb-0"><?php the_field('external_link_notification', 'options'); ?></p>
-					</div>
-					<div class="modal-footer">
-						<a id="externalLink" href="#" class="btn btn-primary btn-block"><?php _e('Proceed','csdschools'); ?></a>
-					</div>
-				</div>
-			</div>
-		</div>		
+		
 		<?php wp_footer(); ?>
 		
 		<div id="google_translate_element" class="d-none"></div>
