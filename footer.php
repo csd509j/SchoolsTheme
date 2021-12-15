e47eb381e3521eff36ef094438e068dddf576683
Josh Armentano <josh@abidewebdesign.com>
2021-12-15 14:10:19 -0800

    no message

--- footer.php
+++ footer.php
@@ -126,7 +126,12 @@
 								</p>
 							</div>
 							<div class="col-sm-5">
-								<p class="footer-text"><?php the_field('statement', 'options'); ?> <?php the_field('statement_contact_name', 'options'); ?> <?php the_field('statement_contact_title', 'options'); ?>, <a href="mailto:<?php the_field('statement_contact_email', 'options'); ?>"><?php the_field('statement_contact_email', 'options'); ?></a> <?php the_field('statement_contact_phone', 'options'); ?></p>
+								<div class="footer-text text-white">
+									<p class="mb-0"><?php the_field('statement', 'options'); ?></p>
+									<p class="mb-0"><?php the_field('statement_contact_name', 'options'); ?></p>
+									<div><?php the_field('statement_contact_title', 'options'); ?></div>
+									<p class="mb-0"><a href="mailto:<?php the_field('statement_contact_email', 'options'); ?>" target="_blank"><?php the_field('statement_contact_email', 'options'); ?></a> <?php the_field('statement_contact_phone', 'options'); ?></p>
+								</div>
 							</div>
 						</div>
 					</div>
